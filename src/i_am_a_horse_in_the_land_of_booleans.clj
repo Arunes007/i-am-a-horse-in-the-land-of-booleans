8d2f2b40ab986aa0b63a4240627bb227861bff3f
root <root@onlyme.(none)>
2015-12-25 08:31:32 +0530

    completed exercises to _am_a_horse_in_the_land_of_booleans

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,77 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
-(defn boolean [x]
-  ":(")
+(defn boolean 
+  "gives true or false based on value"
+   [x]
+   (if x
+      true
+      false
+    ))
 
-(defn abs [x]
-  ":(")
+(defn abs
+  "returns magnitude of a number"
+   [x]
+   (if (< x 0)
+     (- x (* x 2))
+     x
+     )
+  )
 
-(defn divides? [divisor n]
-  ":(")
+(defn divides? 
+  "checks if no is divisible"
+  [divisor n]
+  (if (= 0 (mod  n divisor))
+         true
+         false
+    )
+  )
 
-(defn fizzbuzz [n]
-  ":(")
+(defn fizzbuzz
+"divisibility check of 3 and 5 and 15"
+    [n]
+    (cond
+          (divides? 15 n) "gotcha!"
+          (divides? 5 n) "buzz"
+          (divides? 3 n) "fizz"
+          :else ""
+    )
+  )
 
-(defn teen? [age]
-  ":(")
+(defn teen?
+  "returns true if age is greater than 12 and less than 20"
+     [age]
+     (if (<= 13 age 19)
+            true
+            false
+      )
+  )
 
 (defn not-teen? [age]
-  ":(")
+  (or (not (teen? age)) false)
+  )
 
-(defn generic-doublificate [x]
-  ":(")
+(defn generic-doublificate
+ "return double for no"
+    [n]
+    (cond
+          (number? n) (* n 2)
+          (empty? n) nil
+          (list? n) (* (count n) 2)
+          (vector? n) (* (count n) 2)
+          :else true
+          )
+  )
 
-(defn leap-year? [year]
-  ":(")
+(defn leap-year?
+  "checks whether a year is leap year or not"
+  [year]
+  (cond
+    ( not ( divides? 4 year ) ) false
+    ( divides? 400 year ) true
+    ( divides? 100 year ) false
+    :else true
+  )
+)
 
 ; '_______'
